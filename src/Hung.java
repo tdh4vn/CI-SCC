bdc5a0c09ab6111e7c5c2119df60ea0df5351863
HungTran <tdh4vn@gmail.com>
2016-07-09 14:09:55 +0700

    GameWindow

--- src/Hung.java
+++
@@ -1,8 +0,0 @@
-/**
- * jsadlkajsdklasd
- * asdasdaslkjfasd
- * asdkajsdhakjs
- */
-public class Hung {
-
-}
---
+++ src/com/GameWindow.java
@@ -0,0 +1,7 @@
+package com;
+
+/**
+ * Created by hungtran on 7/9/16.
+ */
+public class GameWindow  {
+}
