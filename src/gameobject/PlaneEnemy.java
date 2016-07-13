beeba7619414547d3970a4810e2f119d02aab7b3
HungTran <tdh4vn@gmail.com>
2016-07-13 14:32:09 +0700

    Sprite Sheet

--- src/gameobject/PlaneEnemy.java
+++ src/gameobject/PlaneEnemy.java
@@ -2,12 +2,28 @@ package gameobject;
 
 import intef.QPressListener;
 
+import java.awt.*;
+
 /**
  * Created by hungtran on 7/12/16.
  */
 public class PlaneEnemy extends Plane implements QPressListener{
+    Animation animation;
     public PlaneEnemy(int x, int y, int type){
         super(x,y,type);
+        animation = new Animation("Resource/cat.png",512, 256,31);
+    }
+
+    @Override
+    public void update() {
+        super.update();
+        animation.update();
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        super.draw(g);
+        animation.draw(g, getPositionX(), getPositionY());
     }
 
     @Override
